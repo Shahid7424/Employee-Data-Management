456e25c41de7704dc7a8943661631314764e4cb6
Shahid shah <114140444+Shahid7424@users.noreply.github.com>
2024-03-06 15:11:39 +0530

    adding files

---
+++ src/com/Shahid/EmployeeDataManagement/DBConnection.java
@@ -0,0 +1,33 @@
+package com.Shahid.EmployeeDataManagement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+
+    static Connection con;
+
+    public static Connection createDBConnection() {
+        return con;
+
+
+        try {
+            //load driver
+            Class.forName("com.mysql.jdbc.Driver");
+            //get connection
+            String url = "jdbc:mysql:  //localhost:3306/employeeDB?useSSL=false";
+            String username = "shahid";
+            String password = "pass_1234";
+            con = DriverManager.getConnection(url, username, password);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return con;
+
+    }
+
+}
+
+
